2320b03ffc7f85f62bff5f389f7da10a0b8868bb
Toncho Lozev <toncho.lozev@yahoo.com>
2018-01-13 01:10:09 +0200

    Update AnimeBindingModel.java

--- Java/Exams/Anime-List/src/main/java/animelist/bindingModel/AnimeBindingModel.java
+++ Java/Exams/Anime-List/src/main/java/animelist/bindingModel/AnimeBindingModel.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class AnimeBindingModel {
-    //TODO: Implement me ...
 
     @NotNull
     @Size(min=1)
