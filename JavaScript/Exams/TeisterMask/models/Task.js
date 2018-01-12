3338a4870e5855f2b69075774701e6007e570629
Toncho Lozev <toncho.lozev@yahoo.com>
2018-01-13 01:07:03 +0200

    Update Task.js

--- JavaScript/Exams/TeisterMask/models/Task.js
+++ JavaScript/Exams/TeisterMask/models/Task.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 let taskSchema = mongoose.Schema({
-	// TODO: Implement me...
 
     title: {type: 'string', required: 'true'},
     status: {type: 'string', required: 'true'},
