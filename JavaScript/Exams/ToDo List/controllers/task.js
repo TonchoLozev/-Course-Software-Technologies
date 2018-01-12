06cf4fdcaed6ab69c3697ae5909712eb0642c2ef
Toncho Lozev <toncho.lozev@yahoo.com>
2018-01-13 01:07:28 +0200

    Update task.js

--- JavaScript/Exams/ToDo List/controllers/task.js
+++ JavaScript/Exams/ToDo List/controllers/task.js
@@ -2,19 +2,14 @@ const Task = require('../models/Task');
 
 module.exports = {
     index: (req, res) => {
-        //TODO: Implement me...
 		res.render('task/index', {'tasks': null});
     },
 	createGet: (req, res) => {
-        //TODO: Implement me...
 	},
 	createPost: (req, res) => {
-        //TODO: Implement me...
 	},
 	deleteGet: (req, res) => {
-        //TODO: Implement me...
 	},
 	deletePost: (req, res) => {
-        //TODO: Implement me...
 	}
 };
