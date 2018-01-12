4dad7b678590887602adf6efe347ab92d9e1ae15
Toncho Lozev <toncho.lozev@yahoo.com>
2018-01-13 01:13:26 +0200

    Update Report.java

--- Java/Exams/Log-Noziroh-Description/src/main/java/lognoziroh/entity/Report.java
+++ Java/Exams/Log-Noziroh-Description/src/main/java/lognoziroh/entity/Report.java
@@ -5,7 +5,6 @@ import javax.persistence.*;
 @Entity
 @Table(name = "reports")
 public class Report {
-    //TODO: Implement me ...
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
